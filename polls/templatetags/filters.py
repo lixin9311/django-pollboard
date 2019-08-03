a8020fe24ff2add45f1219f281e30b2313e85b2e
lucus <lixin9311@gmail.com>
2019-08-03 03:37:33 +0000

    Remove debug print

--- polls/templatetags/filters.py
+++ polls/templatetags/filters.py
@@ -9,14 +9,8 @@ register = template.Library()
 
 @register.filter
 def getopt(choice, name):
-    print('---------------------------------')
-    print(choice)
-    print(name)
-    print('---------------------------------')
     try:
         vote = choice.vote_set.get(name=name)
-        print(vote)
-        print('---------------------------------')
     except (KeyError, Vote.DoesNotExist):
         return 'X'
     else:
